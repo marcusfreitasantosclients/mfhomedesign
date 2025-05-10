9d4ddf39c54a116eda79e95b55488ef29f41839c
Marcus Freitas <marcusfreitasantos@gmail.com>
2025-05-10 11:32:03 -0300

    [FEAT] Removed header image from product-single

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1,24 +1,5 @@
 <?php get_header(); ?>
 
-<?php
-global $product;
-   
-//ACFs
-$header_img = get_field("banner_field");
-?>
-
-<style>
-    body{
-        padding: 0 !important;
-    }
-</style>
-
-<?php if($header_img){ ?>
-    <section class="w-100">
-        <img class="img-fluid w-100" style="height: 50vh; object-fit:cover;" src="<?php echo $header_img['url']; ?>" alt="<?php echo $header_img['alt']; ?>" />
-    </section>
-<?php } ?>
-
 <?php import_component("single-product", [
     "single-product" => [
         "product_id" => get_the_ID()
