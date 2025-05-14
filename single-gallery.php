2e79f60f676ced39ea785e1db8aaa5c629ce3387
Marcus Freitas <marcusfreitasantos@gmail.com>
2025-05-14 20:31:49 -0300

    [FEAT] Gallery single page adjustment

--- single-gallery.php
+++ single-gallery.php
@@ -5,26 +5,30 @@ $post_slug = $post->post_name;
 $gallery_items = get_field('images');
 
 ?>
-<section class="py-5 bg-light">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <?php import_component('simple-text', [
-                        'simple-text' => [
-                            'title' => get_the_title(),
-                            'text' => get_the_content()
-                        ]
-                ]) ?>
-            </div>
 
-            <div class="col-md-8">
-                <div class="mf_gallery_featured_img_wrapper">
-                    <img class="" src="<?= get_the_post_thumbnail_url(get_the_ID()) ?>" alt="" />
+<?php if(get_the_content()){ ?>
+
+    <section class="py-5 bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <?php import_component('simple-text', [
+                            'simple-text' => [
+                                'title' => get_the_title(),
+                                'text' => get_the_content()
+                            ]
+                    ]) ?>
+                </div>
+    
+                <div class="col-md-8">
+                    <div class="mf_gallery_featured_img_wrapper">
+                        <img class="" src="<?= get_the_post_thumbnail_url(get_the_ID()) ?>" alt="" />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php } ?>
 
 
 <?php if(is_array($gallery_items)){ ?>
@@ -33,7 +37,7 @@ $gallery_items = get_field('images');
     
             <div class="mf_gallery_container d-grid gap-1">
                 <?php foreach($gallery_items as $image){ ?>
-                    <div class="">
+                    <div class="mf_gallery_container_img_wrapper">
                         <a href="<?= $image['url'] ?>" data-lightbox="<?= $post_slug ?>" class="mf_gallery_img_wrapper">
                             <img class="img-fluid" src="<?= $image['url'] ?>" alt="<?= $image['alt'] ?>" />
                         </a>
