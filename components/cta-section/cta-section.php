467011b2b32030050992ad2a0183ca48cd3f1a23
Marcus Freitas <marcusfreitasantos@gmail.com>
2025-05-16 18:02:47 -0300

    [FIX] CSS fixes for CTA component

--- components/cta-section/cta-section.php
+++ components/cta-section/cta-section.php
@@ -4,17 +4,20 @@ function mf_cta_section($component_data){
     ?>
     
     <section class="bg-light mf_cta_section">
-            <div class="row align-items-center mf_cta_section_col">
-                <div class="p-5 col-md-3">
-                    <?php import_component('simple-text', [
-                        'simple-text' => [
-                            'title' => $component_data['title'],
-                            'text' => $component_data['text'],
-                            'cta'   => $component_data['call_to_action']
-                        ]
-                    ]) ?>
+            <div class="row align-items-center">
+                <div class="p-0 col-md-3">
+                    <div class="p-5">
+
+                        <?php import_component('simple-text', [
+                            'simple-text' => [
+                                'title' => $component_data['title'],
+                                'text' => $component_data['text'],
+                                'cta'   => $component_data['call_to_action']
+                            ]
+                        ]) ?>
+                    </div>
                 </div>
-                <div class="col-md-9 mf_cta_section_col">
+                <div class="col-md-9 p-0 m-0">
                     <div class="mf_cta_section_img_wrapper">
                         <img class="" src="<?= $site_img['url'] ?>" alt="<?= $site_img['alt'] ?>" />
                     </div>
