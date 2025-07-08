01d1283d5d49ae27655d37985261cae4ced2264f
Marcus Freitas <marcusfreitasantos@gmail.com>
2025-07-08 15:47:13 -0300

    [FEAT] session storage to manage views of whatsapp popup

--- components/whatsapp-modal/whatsapp-modal.js
+++ components/whatsapp-modal/whatsapp-modal.js
@@ -1,8 +1,20 @@
-const closeWhatsappPopup = document.querySelector(
-  ".whatsapp__popup .custom__popup_close"
-);
-const whatsappPopup = document.querySelector(".whatsapp__popup");
+document.addEventListener("DOMContentLoaded", function () {
+  const closeWhatsappPopup = document.querySelector(
+    ".whatsapp__popup .custom__popup_close"
+  );
+  const whatsappPopup = document.querySelector(".whatsapp__popup");
 
-closeWhatsappPopup.addEventListener("click", function () {
-  whatsappPopup.style.display = "none";
+  closeWhatsappPopup.addEventListener("click", function () {
+    whatsappPopup.style.display = "none";
+  });
+
+  const popupViewed = sessionStorage.getItem("whatsapp_popup_viewed");
+
+  if (!popupViewed) {
+    setTimeout(() => {
+      whatsappPopup.classList.add("show__popup");
+    }, 3000);
+
+    sessionStorage.setItem("whatsapp_popup_viewed", true);
+  }
 });
